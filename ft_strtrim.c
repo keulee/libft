8f63d703455c5ba78d70b9a28a06691c9e086298
KE <klee8903@gmail.com>
2019-12-15 19:00:09 +0100

    ft_strtrim.c

---
+++ ft_strtrim.c
@@ -0,0 +1,35 @@
+#include "libft.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+unsigned int	ft_check_set(char const *s1, char c)
+{
+	int		i;
+	char	*set;
+
+	i = 0;
+	set = (char *)s1;
+	while (set[i] != '\0')
+	{
+		if(set[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	unsigned int		i;
+	unsigned int		j;
+
+	i = 0;
+	j = ft_strlen(s1);
+	if (s1 == NULL)
+		return (NULL);
+	while (s1[i] != '\0' && ft_check_set(set, s1[i] == 1))
+		i++;
+	while (ft_check_set(set, s1[j] == 1 && j > i))
+		j--;
+	return (ft_substr(s1, i, j - i + 1));
+}
