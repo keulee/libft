ada38f526e0a2207bb94996445515842716ebb55
keulee <58826218+keulee@users.noreply.github.com>
2019-12-17 20:53:38 +0100

    Update ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -3,25 +3,20 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char			*copy;
-	unsigned char	needle;
 	unsigned int	i;
 	unsigned int	j;
 
 	i = 0;
 	j = 0;
 	copy = NULL;
-	needle = start;
 	if (!(copy = (char *)malloc(sizeof(char) * (len + 1))))
 		return (NULL);
-	while (s[i] != '\0' &&)
+	while (s[i] != '\0')
 	{
-		if (s[i] == needle)
+		while (0 < len)
 		{
-			while (s[i] != '\0' && 0 < len)
-			{
-				copy[j++] = s[i++];
-				len--;
-			}
+			copy[j++] = s[start++];
+			len--;
 		}
 		i++;
 	}
