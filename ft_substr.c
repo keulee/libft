17047d731be77bb7a61f80c2d269d19ff81510f9
keulee <58826218+keulee@users.noreply.github.com>
2019-12-17 21:02:46 +0100

    updated
    문제를 제대로 이해 못해서 코드를 잘못 짬... unsigned int start에서 start는 unsigned char나 char로 cast할 아스키가 아니라 스트링 s에서 index를 이야기 하는 것임.
    <각 파라미터 설명>
    #1. The string from which to create the substring.
    #2. The start index of the substring in the string ’s’.
    #3. The maximum length of the substring.

--- ft_substr.c
+++ ft_substr.c
@@ -23,3 +23,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	copy[j] = '\0';
 	return (copy);
 }
+
+// 문제를 제대로 이해 못해서 코드를 잘못 짬... 
+// unsigned int start에서 start는 unsigned char나 char로 cast할 아스키가 아니라 스트링 s에서 index를 이야기 하는 것임.
+// <각 파라미터 설명>
+// #1. The string from which to create the substring.
+// #2. The start index of the substring in the string ’s’.
+// #3. The maximum length of the substring.
