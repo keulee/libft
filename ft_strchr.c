c42fc37400d26f2fa4f381d9dab8d0d3a12778de
keulee <58826218+keulee@users.noreply.github.com>
2019-12-19 12:09:20 +0100

    ft_strchr.c
    문자열에서 특정한 문자가 가장 먼저 나타나는 곳의 위치를 찾는다.
    이 때, 그 위치를 가리키는 포인터를 리턴한다.
    마지막 NULL 문자도 C 문자열의 일부로 간주하기 때문에 이 함수는 문자열의 맨 끝 부분을 가리키는 포인터를 얻기 위해 사용할 수도 있다.
    c는 검색할 문자로, int 형태로 형변환 되어서 전달되지만 함수 내부적으로는 다시 char 형태로 처리된다.

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,21 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: keulee <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/11 16:45:49 by keulee            #+#    #+#             */
+/*   Updated: 2019/12/11 21:31:19 by keulee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+//
+// Description
+// The strchr() function locates the first occurrence of c (converted to a char) in the string pointed to by s.
+// The terminating null character is considered to be part of the string;
+// therefore if c is `\0', the functions locate the terminating `\0'.
+// The strrchr() function is identical to strchr(), except it locates the last occurrence of c.
+//
+// Return
+// The functions strchr() and strrchr() return a pointer to the located character, 
+// or NULL if the character does not appear in the string.
+//
+#include "libft.h"
 #include <stdlib.h>
 
-char *ft_strchr(const char *s, int c)
+char	*ft_strchr(const char *s, int c)
 {
+	char	*ptr;
 	char	needle;
-	char	*str;
 	int		i;
 
-	i = 0;
 	needle = c;
-	str = (char *)s;
-	while (str[i] != '\0')
+	ptr = (char *)s;
+	i = 0;
+	while (ptr[i] != '\0')
 	{
-		if (str[i] == needle)
-			return (&str[i]);
+		if (ptr[i] == c)
+			return (ptr + i);
 		i++;
 	}
-	if(str[i] == '\0' && c == '\0')
-		return (&str[i]);
+	if (ptr[i] == c)
+		return (ptr + i);
 	return (NULL);
 }
