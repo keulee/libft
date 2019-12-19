e8ef63b8b3eb895c28e5fe7e748a818533399fc2
keulee <58826218+keulee@users.noreply.github.com>
2019-12-19 12:19:45 +0100

    ft_strdup.c
    src를 copy한 스트링을 메모리 동적할당한다.
    copy된 스트링이 리턴되며, 메모리가 제대로 할당되지 않으면 NULL이 리턴된다.

---
+++ ft_strdup.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: keulee <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/16 14:14:10 by keulee            #+#    #+#             */
+/*   Updated: 2019/12/17 19:09:28 by keulee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+//
+// Description
+// The strdup() function allocates sufficient memory for a copy of the string s1, does the copy, and returns a pointer to it.
+// The pointer may subsequently be used as an argument to the function free(3).
+// If insufficient memory is available, NULL is returned and errno is set to ENOMEM.
+//
+#include "libft.h"
+#include <stdlib.h>
+
+char		*ft_strdup(const char *src)
+{
+	char	*copy;
+	int		i;
+	int		len;
+
+	i = 0;
+	copy = NULL;
+	len = ft_strlen(src);
+	if (!(copy = (char *)malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	while (i < len)
+	{
+		copy[i] = src[i];
+		i++;
+	}
+	copy[i] = '\0';
+	return (copy);
+}
