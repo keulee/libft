cfecff85a2243dba0e93147769112729b547c7ba
keulee <58826218+keulee@users.noreply.github.com>
2019-12-21 12:59:14 +0100

    ft_strtrim.c
    substr, strdup을 수정한 다음 하는 commit.
    ft_check_set으로 s1의 처음부터 끝까지, 끝부터 처음까지 set이 있는지 아닌지를 체크하고, 없는 곳이 있다면 그곳의 주소를 각각 리턴했다.

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,6 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: keulee <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/14 15:35:15 by keulee            #+#    #+#             */
+/*   Updated: 2019/12/19 10:22:19 by keulee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+//
+// Description
+// Allocates (with malloc(3)) and returns a copy of ’s1’ with the characters specified in ’set’ 
+// removed from the beginning and the end of the string.
+//
+// Parameters
+// #1. The string to be trimmed.
+// #2. The reference set of characters to trim.
+//
+// Return
+// The trimmed string. NULL if the allocation fails.
+//
+// External functs.
+// Malloc
+//
 #include "libft.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 unsigned int	ft_check_set(char const *s1, char c)
 {
@@ -11,25 +36,27 @@ unsigned int	ft_check_set(char const *s1, char c)
 	set = (char *)s1;
 	while (set[i] != '\0')
 	{
-		if(set[i] == c)
+		if (set[i] == c)
 			return (1);
 		i++;
 	}
 	return (0);
 }
 
-char	*ft_strtrim(char const *s1, char const *set)
+char			*ft_strtrim(char const *s1, char const *set)
 {
 	unsigned int		i;
 	unsigned int		j;
 
 	i = 0;
-	j = ft_strlen(s1);
-	if (s1 == NULL)
-		return (NULL);
-	while (s1[i] != '\0' && ft_check_set(set, s1[i] == 1))
+	j = ft_strlen(s1) - 1;
+	if (*s1 == 0)
+		return (ft_strdup((char *)s1));
+	if (*set == 0)
+		return (ft_strdup(s1));
+	while (s1[i] != '\0' && ft_check_set(set, s1[i]) == 1)
 		i++;
-	while (ft_check_set(set, s1[j] == 1 && j > i))
+	while (ft_check_set(set, s1[j]) == 1 && j > i)
 		j--;
 	return (ft_substr(s1, i, j - i + 1));
 }
