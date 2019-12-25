0ad35e1c484f78def5558ee955f4b09f4c7e114a
keulee <58826218+keulee@users.noreply.github.com>
2019-12-25 17:51:07 +0100

    ft_split.c
    이전에 실수했던 부분 : split 해야 할 char이 당연히 중간에 들어가있을 거라고 생각했다. 그래서 첫 함수에서 "bonjour les amis"같은 구문 경우 sep이 빈칸이라고 했을 때, 이중배열의 개수가 빈칸의 개수  + 1 (count + 1)이라고 생각하고 리턴 값을 매겼다. 하지만 "     bonjour les amis           "같은 문장도 있을 수 있다. 
    최종적으로 그 부분을 수정해서 제대로 된 결과를 도출했다.

--- ft_split.c
+++ ft_split.c
@@ -1,19 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: keulee <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/14 15:24:39 by keulee            #+#    #+#             */
+/*   Updated: 2019/12/25 17:37:00 by keulee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+//
+// Description
+// Allocates (with malloc(3)) and returns an array of strings obtained by splitting ’s’
+// using the character ’c’ as a delimiter. The array must be ended by a NULL pointer.
+//
+// Parameters
+// #1. The string to be split.
+// #2. The delimiter character.
+//
+// Return
+// The array of new strings resulting from the split.
+// NULL if the allocation fails.
+//
+// External functs.
+// malloc, free
+//
+#include "libft.h"
 #include <stdlib.h>
 
 int		ft_tab_count(char const *s, char c)
 {
 	int		i;
 	int		count;
+	int		tmp;
 
 	i = 0;
 	count = 0;
+	tmp = 0;
+	if (s == '\0')
+		return (0);
 	while (s[i] != '\0')
 	{
 		if (s[i] == c)
+			tmp = 0;
+		else if (tmp == 0)
+		{
+			tmp = 1;
 			count++;
+		}
 		i++;
 	}
-	return (count + 1);
+	return (count);
 }
 
 int		ft_tab_len(char const *s, char c, int i)
@@ -33,8 +70,8 @@ char	**ft_tab_free(char const **s, int i)
 {
 	while (i > 0)
 	{
-		free((void *)s);
 		i--;
+		free((void *)s[i]);
 	}
 	free(s);
 	return (NULL);
@@ -44,27 +81,27 @@ char	**ft_split(char const *s, char c)
 {
 	int		i;
 	int		j;
-	int		l;
+	int		k;
 	char	**new;
 
 	i = 0;
 	j = 0;
-	if (s == NULL)
-		return (NULL);
 	if (!(new = (char **)malloc(sizeof(char *) * (ft_tab_count(s, c) + 1))))
+		return (NULL);
+	if (s == NULL)
 		return (NULL);
 	while (s[i] != '\0' && j < ft_tab_count(s, c))
 	{
-		while(s[i] == c)
+		while (s[i] == c)
 			i++;
-		l = 0;
-		if(!(new[j] = (char *)malloc(sizeof(char) * ft_tab_len(s, c, i) + 1)))
+		k = 0;
+		if (!(new[j] = (char *)malloc(ft_tab_len(s, c, i) + 1)))
 			return (ft_tab_free((char const **)new, j));
 		while (s[i] != '\0' && s[i] != c)
-			new[j][l++] = s[i++];
-		new[j][l] = '\0';
+			new[j][k++] = s[i++];
+		new[j][k] = '\0';
 		j++;
 	}
-	new[j] = '\0';
+	new[j] = 0;
 	return (new);
 }
