db4365dfe78c63c1dbfd021803e892c70662dc36
keulee <58826218+keulee@users.noreply.github.com>
2020-01-06 22:35:15 +0100

    ft_lstlast.c

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -20,11 +20,12 @@
 // Last element of the list.
 //
 #include "libft.h"
+#include <stdlib.h>
 
 t_list		*ft_lstlast(t_list *lst)
 {
-    if (lst == NULL)
-        return (NULL);
+	if (lst == NULL)
+		return (NULL);
 	while (lst->next != NULL)
 		lst = lst->next;
 	return (lst);
